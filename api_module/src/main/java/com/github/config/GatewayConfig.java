af98b2b2df8863cc3b6f6b8fa8c89a8ab366637c
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2024-04-23 16:13:42 +0900

    refactor : docker push & CICD 진행중

--- api_module/src/main/java/com/github/config/GatewayConfig.java
+++ api_module/src/main/java/com/github/config/GatewayConfig.java
@@ -8,17 +8,29 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+//    @Bean
+//    public RouteLocator gatewayRoutes (RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route(r -> r.path("/v1/user/**")
+//                        .uri("http://localhost:8081"))
+//                .route(r -> r.path("/v1/video/**")
+//                        //로그인 안한 사용자의 시청기록도 UUID로 관리 //없어도 헤더가 잘 포워딩 된다.
+//                        //.filters(f -> f.addRequestHeader("X-Device-UUID", ""))
+//                        .uri("http://localhost:8082"))
+//                .route(r -> r.path("/v1/advertisement/**")
+//                        .uri("http://localhost:8083"))
+//                .build();
+//    }
+
     @Bean
-    public RouteLocator gatewayRoutes (RouteLocatorBuilder builder){
+    public RouteLocator gatewayRoutes (RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/v1/user/**")
-                        .uri("http://localhost:8081"))
+                        .uri("http://user_module:8081"))
                 .route(r -> r.path("/v1/video/**")
-                        //로그인 안한 사용자의 시청기록도 UUID로 관리 //없어도 헤더가 잘 포워딩 된다.
-                        //.filters(f -> f.addRequestHeader("X-Device-UUID", ""))
-                        .uri("http://localhost:8082"))
+                        .uri("http://video_module:8082"))
                 .route(r -> r.path("/v1/advertisement/**")
-                        .uri("http://localhost:8083"))
+                        .uri("http://advertisement_module:8083"))
                 .build();
     }
 }
