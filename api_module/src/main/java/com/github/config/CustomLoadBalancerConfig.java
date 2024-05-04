305fde969b629f3e7ff0719ab6074c8a7c42b577
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2024-05-04 12:23:34 +0900

    feat_api: 라운드 로빈 동작 확인!

---
+++ api_module/src/main/java/com/github/config/CustomLoadBalancerConfig.java
@@ -0,0 +1,29 @@
+package com.github.config;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@LoadBalancerClients({
+        @LoadBalancerClient(name = "WATCH-HISTORY", configuration = CustomLoadBalancerConfig.class),
+        @LoadBalancerClient(name = "VIDEO-INFO", configuration = CustomLoadBalancerConfig.class),
+        @LoadBalancerClient(name = "VIDEO", configuration = CustomLoadBalancerConfig.class)
+})
+public class CustomLoadBalancerConfig {
+
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> roundRobinLoadBalancer(Environment environment,
+                                                                   LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RoundRobinLoadBalancer(loadBalancerClientFactory
+                .getLazyProvider(name, ServiceInstanceListSupplier.class), name);
+    }
+}
